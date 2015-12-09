a2f1c03ca82966ca09e507d7b86cb28c6769735e
WesselAmmerlaan <a.wessel@live.nl>
2015-12-09 09:55:52 +0100

    Can now read files and convert to documents

--- src/InteractiveLearner/Model/Document.java
+++ src/InteractiveLearner/Model/Document.java
@@ -5,12 +5,26 @@ import java.io.IOException;
 
 public class Document {
 	private BufferedReader in;
+	private String filePath;
+	private String contents;
+	
+	public Document(String fileName) {
+		this.filePath = fileName;
+		this.contents = "";
+		this.readFile(filePath); 
+	}
+	
+	public String getContents() {
+		return this.contents;
+	}
+	
 	public void readFile(String filename) {
 		String l;
 		try {
 			in = new BufferedReader(new FileReader(filename));
 			while ((l = in.readLine()) != null) {
-				//TODO waar zetten we dit in?
+				//this.contents = l;
+				this.contents = contents.concat(l);
 			}
 			in.close();
 		} catch (IOException e1) {
--- src/Learner/Corpus.java
+++ src/Learner/Corpus.java
@@ -1,4 +1,13 @@
 package Learner;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import InteractiveLearner.Model.Document;
+
 /**
  * this is a file containing all the documents
  * this file can be initially generated or replaced by a file input stream
@@ -6,6 +15,27 @@ package Learner;
  *
  */
 public class Corpus {
+	
+	private List<Document> allDocuments;
+	
+	public Corpus(String path) {
+		this.allDocuments = new ArrayList<Document>();
+		
+		try {
+			Files.walk(Paths.get(path)).forEach(filePath -> {
+			    if (Files.isRegularFile(filePath)) {
+			        allDocuments.add(new Document(filePath.toString()));
+			    }
+			});
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Document> getDocuments() {
+		return this.allDocuments;
+	}
 	
 	public static Vocabulary extractVocabulary(Corpus crps) {
 		//TODO extracts all the words from a corpus and adds them in a library
@@ -23,4 +53,15 @@ public class Corpus {
 	public static String ConcatenateAllTextsOfDocsInClass(Corpus crps, String cls) {
 		return null;
 	}
+	
+//	public static void main(String[] args) {
+//		Corpus c =  new Corpus("C:/Users/wessel/Documents/School/2015-2016"
+//				+ "/Module 6 - Intelligent Interaction Design/Artificial Intelligence"
+//				+ "/Interactive Learner/corpus/part1");
+//		int i = 0;
+//		for (Document d : c.getDocuments()) {
+//			i++;
+//			System.out.println(i + " " + d.getContents());
+//		}
+//	}
 }
